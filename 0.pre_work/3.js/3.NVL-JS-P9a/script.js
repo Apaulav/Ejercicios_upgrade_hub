b837fee43b607025f5984d49ac121a412b831cfd
Apaulav <paulaville@gmail.com>
2019-06-09 00:02:56 +0200

    ejercicios

---
+++ 0.pre_work/3.js/3.NVL-JS-P9a/script.js
@@ -0,0 +1,25 @@
+/* 
+Crea un array donde cada elemento sea un lista de dos valores.
+
+Indica la longitud del array
+Indica la longitud de uno de los elementos del array
+Muestra por pantalla el valor del tercer elemento de la lista del array. Y dentro de ese tercer elemento, su segundo elemento. 
+ */
+
+var a =[[gazpacho,arroz],[moneda,billete],[cama,sofa]];
+
+//ejercicio 1.
+alert(a.length);
+//devuelve 3.
+
+//Ejercicio 2
+alert(a[0].length);
+//devuelve 2.
+alert(a[1].length);
+//devuelve 2.
+alert(a[2].length);
+//devuelve 2.
+
+//Ejercicio 3.
+alert(a[2,1]);
+//devuelve sofa.
---
+++ 0.pre_work/3.js/4.NVL-JS-P9b/script.js
@@ -0,0 +1,12 @@
+/* 
+Tu grupo de música está teniendo un éxito bárbaro. Por lo que os han propuesto un listado de conciertos para la semana que viene. La agencia que os provee los datos es un poco caótica y os los pasa en distintos formatos de cadena.
+
+Dados los siguientes valores en dos variables de cadena:
+
+    Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo
+    Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti
+
+Crea un array donde se guarde en cada celda como primer elemento el día de la semana y como segundo el lugar donde se realiza el concierto.
+ */
+
+var a = [[Lunes,Madrid],[Martes,Sevilla],[Miercoles,Barcelona],[Jueves,Valencia],[Viernes,Zaragoza],[Sabado,Bilbao],[Domingo,Donosti]];
