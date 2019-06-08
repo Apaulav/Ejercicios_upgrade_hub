c2fb4a66ebcdbed74e38338862551658072f8eb6
Apaulav <paulaville@gmail.com>
2019-06-08 23:46:19 +0200

    NVL-JS-P8a

--- 0.pre_work/3.js/1.NVL-JS-P6a/script.js
+++ 0.pre_work/3.js/1.NVL-JS-P6a/script.js
@@ -1,5 +1,5 @@
 var nombreCompleto = "Paula Villegas Castro";
 var edad = 33;
-var diasSemana = "Lunes, " + "martes, " + "miércoles, " + "jueves, " + "viernes, " + "sábado " + "y domingo.";
+var diasSemana = [Lunes, martes, miércoles, jueves, viernes, sábado, domingo];
 var valorGravedad = 9.807 + " m/s²";
 var numerosImpares = [1, 3, 5];
---
+++ 0.pre_work/3.js/2.NVL-JS-P8a/script.js
@@ -0,0 +1,5 @@
+//Dada la cadena “Amigo”, conviértela a la cadena “amigo” de tres formas distintas.
+a = "Amigo";
+console.log(a.toLowerCase());
+console.log(a.replace('A','a'));
+console.log("a" + a.slice(1,5));
